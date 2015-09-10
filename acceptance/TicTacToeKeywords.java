71c1bae0ded001c50cf47e6a2f3d237faed5353f
batmantotoro <bhoskins@stu.jsu.edu>
2015-09-09 22:23:08 -0500

    af;kjdsan;adkfjnafd

--- acceptance/TicTacToeKeywords.java
+++ acceptance/TicTacToeKeywords.java
@@ -2,19 +2,28 @@ import edu.jsu.mcis.*;
 
 public class TicTacToeKeywords {
 	
+	private TicTacToe t;
+	
 	public void startNewGame() {
-
+		t = new TicTacToe();
 	}
 	
 	public void markLocation(int row, int col) {
-
+		t.setMark(row, col);
 	}
 	
 	public String getMark(int row, int col) {
-		return "";
+		TicTacToe.Mark m = t.getMark(row,col);
+		if(m == TicTacToe.Mark.XMARK) return "X";
+		else if(m == TicTacToe.Mark.OMARK) return "O";
+		else return "";
 	}
     
 	public String getWinner() {
-		return "";
+		TicTacToe.gameStatus s = t.getStatus();
+		if(s == TicTacToe.gameStatus.XWIN) return "X";
+		else if(s == TicTacToe.gameStatus.OWIN) return "O";
+		else if(s == TicTacToe.gameStatus.DRAW) return "TIE";
+		else return "";
 	}
 }
